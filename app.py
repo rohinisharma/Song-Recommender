a074ba0fbf8e13cce6a89ddff955ab5d08d7838e
rohinisharma <rasberry314@gmail.com>
2020-08-04 12:23:54 -0400

    Added recommendation functionality

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, redirect, url_for, json
+from flask import Flask, request, redirect, url_for, json, session
 import flask
 import wtforms
 import requests
+import random
 import musicbrainzngs as mb
 from form import SongForm
+from collections import Counter
 import mysql.connector
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
@@ -26,14 +28,41 @@ LAST_FM= "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api}&a
 
 @app.route('/', methods = ['GET','POST'])
 def index():
-    if 'add' in request.form:
-        return flask.render_template("./add_song.html")
+    session['recs'] = []
     if 'list' in request.form:
         return redirect(url_for('get_likes'))
+    if 'recs' in request.form:
+        return redirect(url_for('show_recs'))
     elif request.method == 'GET':
         return flask.render_template("./index.html")
 
-@app.route('/add_like', methods = ['POST'])
+@app.route('/show_recs', methods = ['GET','POST'])
+def show_recs():
+    if 'generate' in request.form:
+        return redirect(url_for('generate_rec'))
+    if request.method == 'GET':
+        return flask.render_template('./recommendations.html')
+    
+
+@app.route('/generate_rec', methods = ['GET','POST'])
+def generate_rec():
+    if len(session.get('recs')) == 0:
+        generate_recommendations('rsharma')
+    recs = session.get('recs')
+    if len(recs) == 0:
+        recs = session['seen']
+        session['seen'] = []
+    song_id = random.choice(recs)
+    if session.get('seen') == None:
+        session['seen'] = [song_id]
+    else: 
+        session['seen'].append(song_id)
+    recs.remove(song_id)
+    session['recs'] = recs
+    song_info = get_song_from_id(song_id[0])
+    message = "{title} by {artist}".format(title=song_info[0], artist=song_info[1])
+    return flask.render_template('./recommendations.html', message=message)
+
 
 
 @app.route('/get_likes', methods = ['GET','POST'])
@@ -71,7 +100,7 @@ def remove_like():
     data = json.loads(request.data)
     for d in data:
         remove_like_from_db(d[0], d[1])
-    return "hi"
+    return "Removed likes"
 
 
 def remove_like_from_db(title, artist):
@@ -109,10 +138,6 @@ def add_like(title, artist, tag):
     add_like_to_db(song_id)
     message = "Added {song} by {artist}".format(song = resp["track"]["name"], artist = resp["track"]["artist"]["name"])
     return message
-
-
-
-
 
 def add_user(metadata):
     cursor = mydb.cursor()
@@ -182,6 +207,82 @@ def update_tag(title, artist, new_tag):
     message = "Changed tag for {title} by {artist} from {old} to {new}".format(title=title, artist=artist, old=result[0], new=new_tag)
     return message
 
+def get_song_from_id(id_):
+   cursor = mydb.cursor() 
+   sql = "SELECT Title, Artist FROM Songs WHERE SongId = %s"
+   val = (id_,)
+   cursor.execute(sql,val)
+   result = cursor.fetchone() 
+   cursor.close()
+   return result
+
+def generate_recommendations(user_id):
+    print('hello')
+    cursor = mydb.cursor()
+    sql = "SELECT u.Username, COUNT(SongId) FROM Likes l, Users u WHERE l.Username = %s AND SongId IN (SELECT SongId FROM Likes WHERE Likes.Username = u.Username AND Likes.Username <> %s) GROUP BY u.Username ORDER BY COUNT(SongId) DESC LIMIT 10;"
+    val = (user_id, user_id)
+    cursor.execute(sql,val)
+    result = cursor.fetchall()
+    if len(result) == 0:
+        top_tag = get_top_tag(user_id)
+        sql = "SELECT SongId FROM Songs WHERE tag = %s AND SongId NOT IN (SELECT SongId FROM Likes WHERE Username = %s) LIMIT 5"
+        val = (top_tag,user_id)
+        cursor.execute(sql,val)
+        result = cursor.fetchall()
+        session['recs'] = result
+        return
+    scores = {}
+    for user in result:
+        scores[user[0]] = generate_similarity_score(user_id, user[0], user[1])
+    most_similar = get_top_three(scores)
+    recs = []
+    for u in most_similar:
+        other_user = u[0]
+        sql = "SELECT SongId FROM Likes l WHERE l.Username = %s AND l.SongId NOT IN (SELECT SongId FROM Likes WHERE Likes.Username = %s)"
+        val = (other_user, user_id)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        recs += result
+    cursor.close()
+    session['recs'] = recs
+
+def generate_similarity_score(current_user, other_user, num_similar_songs):
+    score = (num_similar_songs / get_total_num_likes(current_user)) * 100
+    if get_top_tag(current_user) == get_top_tag(other_user):
+        score += 5
+    if get_top_artist(current_user) == get_top_artist(other_user):
+        score += 10
+
+
+def get_top_three(scores):
+    count = Counter(scores)
+    return count.most_common(3)
+
+def get_top_tag(user):
+    cursor = mydb.cursor()
+    sql = "SELECT tag, COUNT(tag) FROM (SELECT SongId FROM Likes WHERE Username = %s) temp JOIN Songs ON temp.SongId = Songs.SongId GROUP BY tag ORDER BY COUNT(tag) DESC LIMIT 1"
+    val = (user,)
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    return result[0]
+
+def get_top_artist(user):
+    cursor = mydb.cursor()
+    sql = "SELECT Artist, COUNT(Artist) FROM (SELECT SongId FROM Likes WHERE Username = %s) temp JOIN Songs ON temp.SongId = Songs.SongId GROUP BY Artist ORDER BY COUNT(Artist) DESC LIMIT 1"
+
+    val = (user,)
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    return result[0]
+
+def get_total_num_likes(user):
+    cursor = mydb.cursor()
+    sql = "SELECT COUNT(SongId) FROM Likes WHERE Username = %s"
+    val = (user,)
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    return result[0]
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
